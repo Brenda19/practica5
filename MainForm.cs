b6a9b38fb5c03483039fbbfcf19c5a0bb3c8e999
brenda19 <brenda.herrera@alumno.udg.mx>
2013-10-31 16:28:05 -0600

    practica5

---
+++ MainForm.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace prac5
+{
+	
+	public partial class MainForm : Form
+	{
+		double peso, dol, conver;
+		
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+		}
+			
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		void TextBox2TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		void TextBox3TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		
+        void Button1Click(object sender, EventArgs e){
+			
+          if(double.TryParse(textBox1.Text, out peso) &&
+			double.TryParse(textBox2.Text, out conver)
+            && peso!=0 && conver!=0){
+
+         dol = peso/conver;
+         
+		textBox3.Text = dol.ToString();
+        }
+
+       else{
+       MessageBox.Show("Error con los datos");
+			}
+		}
+	}
+}
